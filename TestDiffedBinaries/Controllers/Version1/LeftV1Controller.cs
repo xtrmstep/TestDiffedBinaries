cca6fa86ceea03cdddd33de9e46e6ee56b9fe69f
Alexander Goida <alex.goida@xtrmstep.ru>
2015-12-22 10:27:27 +0300

    urls are created

---
+++ TestDiffedBinaries/Controllers/Version1/LeftV1Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestDiffedBinaries.Api.Controllers.Version1
+{
+    [RoutePrefix("api/v1/diff")]
+    public class LeftV1Controller : ApiController
+    {
+        [Route("left")]
+        public IHttpActionResult Get()
+        {
+            return Ok("left");
+        }
+    }
+}
---
+++ TestDiffedBinaries/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestDiffedBinaries.WebApiApplication" Language="C#" %>
